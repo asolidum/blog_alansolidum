b8a28251bd2fdca6a782bc5cd5a6a92e909ffed8
Alan Solidum <me@alansolidum.com>
2013-06-21 05:28:54 -1000

    static content update

---
+++ _plugins/image_raw.rb
@@ -0,0 +1,21 @@
+module Jekyll
+  class ImageRawTag < Liquid::Tag
+    def initialize(tag_name, text, tokens)
+      super
+      split_text = text.split(',', 2)
+      @url = split_text[0]
+      @text = split_text[1] 
+    end
+
+    def render(context)
+      "<div>
+         <img src='#{@url}'>
+         <div class='text-overlay'>
+           #{@text}
+         </div>
+       </div>"
+    end
+  end
+end
+
+Liquid::Template.register_tag('image_raw', Jekyll::ImageRawTag)
