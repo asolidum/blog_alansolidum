3aee3bbc35aff89c28afdb51dec9fcfaa0ee39a6
Alan Solidum <me@alansolidum.com>
2013-06-25 04:44:41 -1000

    Combined image_column and image_column_no_caption into one tag

--- _plugins/image_column.rb
+++ _plugins/image_column.rb
@@ -4,36 +4,25 @@ module Jekyll
       super
       split_text = text.split(',', 4)
       @num_cols = split_text[0]
-      @offset = split_text[1]
-      @url = split_text[2]
+      @url = split_text[1]
+      @offset = split_text[2]
       @text = split_text[3] 
     end
 
     def render(context)
-      "<div class='large-#{@num_cols} columns image-columns'>
-         <img src='#{@url}'>
+      if @text.nil?
+        img_src = "<img src='#{@url}' style='margin-bottom: 10px;'>"
+      else
+        img_src = "<img src='#{@url}'>
          <div class='text-overlay' style='margin-top: #{@offset}px;'>
            #{@text}
-         </div>
-       </div>"
+         </div>"
+      end
+      "<div class='large-#{@num_cols} columns image-columns'>#{img_src}</div>"
     end
   end
 
-  class ImageColumnNoCaptionTag < Liquid::Tag
-    def initialize(tag_name, text, tokens)
-      super
-      split_text = text.split(',', 2)
-      @num_cols = split_text[0]
-      @url = split_text[1]
-    end
-
-    def render(context)
-      "<div class='large-#{@num_cols} columns image-columns'>
-         <img src='#{@url}' style='margin-bottom: 10px;'>
-       </div>"
-    end
-  end
 end
 
 Liquid::Template.register_tag('image_column', Jekyll::ImageColumnTag)
-Liquid::Template.register_tag('image_column_no_caption', Jekyll::ImageColumnNoCaptionTag)
+
