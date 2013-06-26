cd1674b9ae1a2f320e40e27a02a8648461b501b0
Alan Solidum <me@alansolidum.com>
2013-06-26 05:58:32 -1000

    Changed image_column tag to use hash params and have default values

--- _plugins/image_column.rb
+++ _plugins/image_column.rb
@@ -2,27 +2,24 @@ module Jekyll
   class ImageColumnTag < Liquid::Tag
     def initialize(tag_name, text, tokens)
       super
-      split_text = text.split(',', 4)
-      @num_cols = split_text[0]
-      @url = split_text[1]
-      @offset = split_text[2]
-      @text = split_text[3] 
+      @params = eval('{'+text+'}')
+      @params[:cols] ||= 20
+      @params[:offset] ||= 0
     end
 
     def render(context)
-      if @text.nil?
-        img_src = "<img src='#{@url}' style='margin-bottom: 10px;'>"
+      if @params[:caption].nil?
+        img_src = "<img src='#{@params[:url]}' style='margin-bottom: 10px;'>"
       else
-        img_src = "<img src='#{@url}'>
-         <div class='text-overlay' style='margin-top: #{@offset}px;'>
-           #{@text}
-         </div>"
+        img_src = "<img src='#{@params[:url]}'>
+          <div class='text-overlay' style='margin-top: #{@params[:offset]}px;'>
+            #{@params[:caption]}
+          </div>"
       end
-      "<div class='large-#{@num_cols} columns image-columns'>#{img_src}</div>"
+      "<div class='large-#{@params[:cols]} columns image-columns'>#{img_src}</div>"
     end
   end
 
 end
 
 Liquid::Template.register_tag('image_column', Jekyll::ImageColumnTag)
-
