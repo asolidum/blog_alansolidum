65dd9f0c440f0ecc7bdea40cba99f3429cb601f5
Alan Solidum <me@alansolidum.com>
2013-06-28 05:40:27 -1000

    Added responsive images in posts

--- _plugins/image_column.rb
+++ _plugins/image_column.rb
@@ -1,25 +1,62 @@
+require 'json'
+require 'net/http'
+require 'uri'
+
 module Jekyll
   class ImageColumnTag < Liquid::Tag
     def initialize(tag_name, text, tokens)
       super
+      # Using eval okay cause we precompile the site before deploying
       @params = eval('{'+text+'}')
+      
       @params[:cols] ||= 20
       @params[:offset] ||= 0
+      @params[:flickr_id] ||= "9131978755"
+
+      @spans = get_flickr_spans(@params[:flickr_id])
     end
 
     def render(context)
       if @params[:caption].nil?
-        img_src = "<img src='#{@params[:url]}' style='margin-bottom: 10px;'>"
+        img_spans = @spans+"<div style='margin-bottom: 10px;'></div>"
       else
-        img_src = "<img src='#{@params[:url]}'>
-          <div class='text-overlay' style='margin-top: #{@params[:offset]}px;'>
+        img_spans = @spans+
+          "<div class='text-overlay' style='margin-top: #{@params[:offset]}px;'>
             #{@params[:caption]}
           </div>"
       end
-      "<div class='large-#{@params[:cols]} columns image-columns'>#{img_src}</div>"
+      "<div class='large-#{@params[:cols]} columns image-columns'>#{img_spans}</div>"
+    end
+
+    def get_flickr_spans(id)
+      api_key = "326962bdfea628002a66395ff0fb4251"
+      uri = "http://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=#{api_key}&photo_id=#{id}&format=json"
+      resp = Net::HTTP.get(URI.parse(uri))
+      parsed = JSON.parse(resp[14..-2])
+      images = parsed["sizes"]["size"]
+      span = "<span data-picture>"
+      span += get_flickr_image_span(images[4])
+      span += get_flickr_image_span(images[7])
+      if !images[10].nil?
+        span += get_flickr_image_span(images[10])
+      end
+      span += "</span>"
+      span
+    end
+
+    def get_flickr_image_span(image)
+      "<span data-src='#{image['source']}' data-media='(min-width: #{image['width']}px)'></span>"
+    end
+
+    def get_image_span(url, width)
+      span = "<span data-src='#{url}'"
+      if !width.nil?
+        span += " data-media='(min-width: #{width}px)'"
+      end
+      span += "></span>"
     end
   end
-
+    
 end
 
 Liquid::Template.register_tag('image_column', Jekyll::ImageColumnTag)
