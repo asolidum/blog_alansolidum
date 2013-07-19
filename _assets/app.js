6d55d0411b7113b220933c943b0ada3e345c5caa
Alan Solidum <me@alansolidum.com>
2013-07-19 11:47:03 -1000

    site content update

--- _assets/app.js
+++
@@ -1,4 +0,0 @@
-//= require vendor/custom.modernizr
-//= require /js/vendor/picturefill
-//= require /js/foundation.min.js
-//= require /js/foundation/foundation.clearing
--- _assets/javascripts/app.js
+++
@@ -1,4 +0,0 @@
-//= require vendor/custom.modernizr
-//= require vendor/picturefill
-//= require foundation.min
-//= require foundation/foundation.clearing
